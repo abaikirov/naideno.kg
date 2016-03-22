67cd067ff2ae9ce17a33b50e3b4b380106f97672
Абакиров Абай Ильясович <abaikirov@outlook.com>
2016-03-22 20:30:04 +0600

    EfDbContext

---
+++ naideno.kg/Context/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using naideno.kg.Models;
+
+namespace naideno.kg.Context
+{
+	public class EFDbContext
+	{
+        DbSet<Passport> Passports { get; set; }
+        DbSet<Universal> Universals { get; set; }
+        DbSet<User> Users { get; set; }
+    }
+}

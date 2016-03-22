0325028c68cc77a997353d21ab5c94a35dbad029
Алексей <grinderbrader@gmail.com>
2016-03-22 09:14:37 -0500

    db init

---
+++ naideno.kg/Models/init.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using naideno.kg.Context;
+using System.Data.Entity;
+
+namespace naideno.kg.Models
+{
+    public class init : DropCreateDatabaseAlways<EFDbContext>
+    {
+        protected override void Seed(EFDbContext db)
+        {
+            db.Users.Add(new User { Name = "buronahodok" });
+            base.Seed(db);
+        }
+    }
+}

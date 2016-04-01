e41fdfa910c8a869f0dd970e3262e673b849256d
Алексей <grinderbrader@gmail.com>
2016-04-01 10:27:35 -0500

    forms for adding adverts

---
+++ naideno.kg/Controllers/addingadvertController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using naideno.kg.Models;
+using naideno.kg.Context;
+
+namespace naideno.kg.Controllers
+{
+    public class addingadvertController : Controller
+    {
+        public EFDbContext db = new EFDbContext(); 
+        // GET: addingadvert
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public ActionResult adddocument()
+        {
+            return PartialView();
+        }
+        public ActionResult adduniversal()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public string adduniversal(Universal thing)
+        {
+            //здесь поиск соответствия
+            db.Universals.Add(thing);
+            db.SaveChanges();
+            return "лоль";
+        }
+        public string adddocument(Passport pass)
+        {
+            //здесь поиск соотвествия
+            db.Passports.Add(pass);
+            db.SaveChanges();
+            return "лоль";
+        }
+    }
+}
